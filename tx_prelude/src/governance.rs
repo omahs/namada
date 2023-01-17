45ffb43eda77903a094474d922e9f7d59742f612
Marco Granelli <marco.granelli@ymail.com>
2023-01-17 14:41:57 +0100

    Updates init proposal tx and governance VP

--- tx_prelude/src/governance.rs
+++ tx_prelude/src/governance.rs
@@ -24,6 +24,17 @@ pub fn init_proposal(ctx: &mut Ctx, data: InitProposalData) -> TxResult {
     let author_key = storage::get_author_key(proposal_id);
     ctx.write(&author_key, data.author.clone())?;
 
+    let proposal_type_key = storage::get_proposal_type_key(proposal_id);
+    match data.r#type {
+        ProposalType::Default(Some(code)) => {
+            // Remove wasm code and write it under a different subkey
+            ctx.write(&proposal_type_key, ProposalType::Default(None))?;
+            let proposal_code_key = storage::get_proposal_code_key(proposal_id);
+            ctx.write_bytes(&proposal_code_key, code)?
+        }
+        _ => ctx.write(&proposal_type_key, data.r#type.clone())?,
+    }
+
     let voting_start_epoch_key =
         storage::get_voting_start_epoch_key(proposal_id);
     ctx.write(&voting_start_epoch_key, data.voting_start_epoch)?;
@@ -33,11 +44,6 @@ pub fn init_proposal(ctx: &mut Ctx, data: InitProposalData) -> TxResult {
 
     let grace_epoch_key = storage::get_grace_epoch_key(proposal_id);
     ctx.write(&grace_epoch_key, data.grace_epoch)?;
-
-    if let ProposalType::Default(Some(proposal_code)) = data.r#type {
-        let proposal_code_key = storage::get_proposal_code_key(proposal_id);
-        ctx.write_bytes(&proposal_code_key, proposal_code)?;
-    }
 
     ctx.write(&counter_key, proposal_id + 1)?;
 
